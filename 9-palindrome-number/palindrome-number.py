19a34483ed18f86079f1a13afddd95c9726545bf
nanda <48620949+nandaisnanda@users.noreply.github.com>
2024-07-01 22:31:05 +0700

    Time: 30 ms (89.62%) | Memory: 11.6 MB (59.32%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        x_string = str(x)
+        return x_string== x_string[::-1]
